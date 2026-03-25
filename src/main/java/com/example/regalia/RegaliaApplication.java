b6565bd4985444f303f597504bdad73e77d287e4
Mun0449 <bunonggum89@gmail.com>
2026-03-25 15:05:59 +0900

    init: project setup

---
+++ src/main/java/com/example/regalia/RegaliaApplication.java
@@ -0,0 +1,13 @@
+package com.example.regalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegaliaApplication.class, args);
+	}
+
+}
